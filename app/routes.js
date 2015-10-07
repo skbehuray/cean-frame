f7fb1a8e960d5ed43fb7adab480a85c4de20a25c
Santosh Behuray <skbehuray@gmail.com>
2015-10-07 23:50:48 +0530

    Code refactor and new routes

--- app/routes.js
+++ app/routes.js
@@ -35,6 +35,31 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/api/employee', function(req, res) {
+    var emp_id = req.param('id');
+    // use Apollo to get all employees in the database
+    Employee.find({empid: emp_id}, function(err, employees) {
+      // if there is an error retrieving, send the error.
+      // nothing after res.send(err) will execute
+      if (err) {
+        res.send(err);
+      }
+      res.json(employees); // return all nerds in JSON format
+    });
+  });
+
+  app.get('/api/employee/:empid', function(req, res) {
+    // use Apollo to get all employees in the database
+    Employee.find({empid: req.params.empid}, function(err, employees) {
+      // if there is an error retrieving, send the error.
+      // nothing after res.send(err) will execute
+      if (err) {
+        res.send(err);
+      }
+      res.json(employees); // return all nerds in JSON format
+    });
+  });
+
   // route to handle creating goes here (app.post)
   // route to handle delete goes here (app.delete)
 
